f12d2fc8eb57cbfd7b10e355d5ac47d867490000
zhangxinwu112 <zhangxinwu112@163.com>
2019-08-13 18:20:34 +0800

    ui

---
+++ pubgClient/Assets/Scripts/MVC/View/Chat/SetBG.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetBG : MonoBehaviour {
+
+    private Transform child;
+
+    private float width;
+    private float height;
+
+    public float widthLength;
+    public float heightLength;
+
+
+    public float maxWidth = 0.0f;
+	void Start ()
+    {
+        child = gameObject.transform.GetChild(0);
+        maxWidth = GetComponent<RectTransform>().sizeDelta.x;
+    }
+
+    void Update ()
+    {
+        width = child.GetComponentInChildren<Text>().preferredWidth;
+        height = child.GetComponentInChildren<Text>().preferredHeight;
+        if(width + widthLength> maxWidth)
+        {
+            GetComponent<RectTransform>().sizeDelta = new Vector2(maxWidth, height + heightLength);
+        }
+        else
+        {
+            GetComponent<RectTransform>().sizeDelta = new Vector2(width + widthLength, height + heightLength);
+        }
+       
+    }
+}
---
+++ pubgClient/Assets/Scripts/MVC/View/Chat/SynchronizationHeight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SynchronizationHeight : MonoBehaviour {
+    [SerializeField]
+    public Transform item;
+
+    private float sizeDataHight = 0.0f;
+
+    private float defaultWidth = 0.0f;
+
+    private void Start()
+    {
+        defaultWidth = GetComponent<RectTransform>().sizeDelta.x;
+    }
+    void Update () {
+        sizeDataHight = 0.0f;
+        foreach(Transform child in item)
+        {
+            sizeDataHight = sizeDataHight + child.GetComponent<RectTransform>().sizeDelta.y;
+        }
+        GetComponent<RectTransform>().sizeDelta = new Vector2(defaultWidth, sizeDataHight);
+            
+    }
+}

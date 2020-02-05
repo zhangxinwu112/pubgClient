dc8d4eee5e27e5181c6fb05bcc146b05b38310fd
zhangxinwu112 <zhangxinwu112@163.com>
2020-02-05 17:14:47 +0800

    commit

---
+++ pubgClient/Assets/Scripts/MVC/View/GrounpStateButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GrounpStateButton : MonoBehaviour {
+
+    [SerializeField]
+    private Sprite StopSprite;
+
+    [SerializeField]
+    private Sprite PlaySprite;
+
+    public bool isPlay = false;
+	void Start () {
+
+        GetComponent<Button>().onClick.AddListener(() => {
+            SwitchState();
+
+        });
+	}
+
+    public void SwitchState()
+    {
+        if(!isPlay)
+        {
+            isPlay = !isPlay;
+
+            GetComponent<Image>().sprite = PlaySprite;
+        }
+    }
+	
+	
+}

71d310b189febc28559cedec5059723985142a22
zhangxinwu112 <zhangxinwu112@163.com>
2020-02-05 22:39:21 +0800

    server

--- pubgClient/Assets/Scripts/MVC/View/GrounpStateButton.cs
+++ pubgClient/Assets/Scripts/MVC/View/GrounpStateButton.cs
@@ -27,14 +27,23 @@ public class GrounpStateButton : MonoBehaviour {
                 {
                     GrounpSataeProxy.SaveState(grounpId, (result) => {
 
-                        GetComponentInParent<RootCreateRoomView>().errorMessage.ShowMessage(grounpName + "，游戏已启动", 5.0f);
-                        SwitchState();
+                        result = result.Trim('"');
+                        if (result.Equals("0"))
+                        {
+                            GetComponentInParent<RootCreateRoomView>().errorMessage.ShowMessage(grounpName + "，游戏已成功启动", 5.0f);
+                            SwitchState();
+                        }
+                        else
+                        {
+                            GetComponentInParent<RootCreateRoomView>().errorMessage.ShowMessage(result,5.0f);
+                        }
+                       
 
                     });
                 }
                 else
                 {
-                    GetComponentInParent<RootCreateRoomView>().errorMessage.ShowMessage(grounpName + "，游戏已处于运行状态", 3.0f);
+                    GetComponentInParent<RootCreateRoomView>().errorMessage.ShowMessage(grounpName + "，游戏已处于运行状态");
                 }
 
 
